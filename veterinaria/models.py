bba89187f9af77f8c1c879c2863628e38f09312f
macarenakcp <macakailenna@gmail.com>
2025-10-07 17:12:12 -0300

    mas cambios

--- veterinaria/models.py
+++ veterinaria/models.py
@@ -6,6 +6,7 @@ class Mascota(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
+    motivo = models.CharField(max_length=200) 
     especie = models.CharField(max_length=50)
 
     
